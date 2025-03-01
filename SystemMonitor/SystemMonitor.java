2d66be10d6d93753f5e86d8097616b69de3afc6e
janak <janakchy01@gmail.com>
2025-03-02 00:06:50 +0545

    added main class to run the program

--- SystemMonitor/SystemMonitor.java
+++ SystemMonitor/SystemMonitor.java
@@ -1,5 +1,28 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class SystemMonitor{
+
     public static void main(String[] args) {
+
+        Scanner sc =new Scanner(System.in);
+        while(true){
+        System.out.println("Enter logfile file path");
+        String logFile=sc.nextLine();
+        
+        File file=new File(logFile);
+        System.out.println(file.getAbsolutePath());
+        final String alertLogFile="alert.log";
+        
+            if(file.exists()){
+                ProcessLogUtils.monitorLogFile(logFile, alertLogFile);
+                sc.close();
+            }
+            else{
+                System.out.println("ERROR: Log file doesn't exist");
+            }    
+        }
+       
         
     }
 }
